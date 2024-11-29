8789b0b43b98d12b73c970c659ee5337f52a53c8
Mays-Alreem <hroubmays@gmail.com>
2024-11-29 15:37:13 +0200

    feat: add min-heap implementations

---
+++ src/main/java/com/msreem/huffmancoding/heap/MinHeapInterface.java
@@ -0,0 +1,12 @@
+package com.msreem.huffmancoding.heap;
+
+public interface MinHeapInterface<T extends Comparable<T>> {
+
+    void add(T data);
+    T removeMin();
+    T getMin();
+    boolean isEmpty();
+    int getSize();
+    void clear();
+
+}
