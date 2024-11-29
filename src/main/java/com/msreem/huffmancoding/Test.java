26e84d34de0ace60f88e8cb15ff9b9bd888a330e
Mays-Alreem <hroubmays@gmail.com>
2024-11-29 16:32:18 +0200

    feat: build huffman tree and print it

--- src/main/java/com/msreem/huffmancoding/Test.java
+++ src/main/java/com/msreem/huffmancoding/Test.java
@@ -1,11 +1,46 @@
 package com.msreem.huffmancoding;
 
+import com.msreem.huffmancoding.heap.MinHeap;
+import com.msreem.huffmancoding.node.HNode;
+
 import java.io.*;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Test {
 
     public static void main(String[] args) throws IOException {
-        testWithTxtFile();
+        int[] freq = testWithTxtFile();
+
+        MinHeap<HNode> minHeap = new MinHeap<>(256);
+
+        int n = 0;
+        for (int i = 0; i < freq.length; i++) {
+            if (freq[i] == 0) continue;
+            minHeap.add(new HNode(freq[i], (byte) i));
+            n++;
+        }
+
+        for (int i = 0; i < n-1; i++) {
+            HNode x = minHeap.removeMin(), y = minHeap.removeMin();
+            HNode z = new HNode(x.getFreq() + y.getFreq());
+            z.setLeft(x);
+            z.setRight(y);
+            minHeap.add(z);
+        }
+
+        // level-order traversal
+        HNode root = minHeap.getMin();
+        Queue<HNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            HNode curr = queue.poll();
+            System.out.print(curr + " ");
+            if (curr.getLeft() != null)
+                queue.offer(curr.getLeft());
+            if (curr.getRight() != null)
+                queue.offer(curr.getRight());
+        }
     }
 
     private static void testWithImg() throws IOException {
@@ -30,7 +65,7 @@ public class Test {
                 System.out.println(i + " : " + freq[i]);
     }
 
-    private static void testWithTxtFile() throws IOException {
+    private static int[] testWithTxtFile() throws IOException {
         final String filePath = "src/main/resources/com/msreem/huffmancoding/RandomCharacters.txt";
         final byte bufferSize = 8;
 
@@ -50,6 +85,8 @@ public class Test {
         for (int i = 0; i < freq.length; i++)
             if (freq[i] != 0)
                 System.out.println((char) (i-128) + " : " + freq[i]);
+
+        return freq;
     }
 
 }
