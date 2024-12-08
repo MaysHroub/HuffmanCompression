04b0f185adc3056cab317be12f9a19e83fa014f9
Mays-Alreem <hroubmays@gmail.com>
2024-12-08 12:37:29 +0200

    build: exports HuffmanData to project

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,10 @@
 module com.msreem.huffmancoding {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
+    opens com.msreem.huffmancoding.tabledata to javafx.base;
+    exports com.msreem.huffmancoding.tabledata;
 
     opens com.msreem.huffmancoding to javafx.fxml;
     exports com.msreem.huffmancoding;
